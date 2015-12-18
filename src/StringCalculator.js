424930e985b1408e51713cef73911c3394f353ef
Oscar <galindero@gmail.com>
2015-12-18 11:11:34 +0100

    Moar spaguetti

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,8 +1,24 @@
 StringCalculator = function() {
-    function add(numbers) {
-        if(!numbers.trim()) return 0;
-        return numbers;
+    function add(string) {
+        if (!string.trim()) return 0;
+        string += ',0'; // lol
+
+        var regex = /\/\/(.*)\n(.*)/g;
+        var match = regex.exec(string);
+
+        var delimiter = (match) ? match[1] : '\n';
+        var numbers = (match) ? match[2] : string;
+
+        var re = new RegExp(delimiter, 'g');
+        numbers = numbers.replace(re, ',');
+
+        return numbers
+            .split(",")
+            .reduce(function(x, y) {
+                return parseInt(x, 10) + parseInt(y, 10);
+            });
     }
+
     return {
         Add: add
     }
--- test/StringCalculator.spec.js
+++ test/StringCalculator.spec.js
@@ -17,7 +17,30 @@ describe("String Calculator", function() {
     it("should return the same number if only pass one", function() {
         var data = "5";
         var expectedResult = stringCalculator.Add(data);
-        expect(expectedResult).to.be.equal("5");
+        expect(expectedResult).to.be.equal(5);
     });
 
+    it("should return the sum of the numbers if it have 2 numbers", function() {
+        var data = "1,2";
+        var expectedResult = stringCalculator.Add(data);
+        expect(expectedResult).to.be.equal(3);
+    });
+
+    it("should return the sum of the numbers if it have more than 3 numbers", function() {
+        var data = "1,2,3";
+        var expectedResult = stringCalculator.Add(data);
+        expect(expectedResult).to.be.equal(6);
+    });
+
+    it("should return the sum if the string have a return line to separate numbers", function() {
+        var data = "1\n2,3";
+        var expectedResult = stringCalculator.Add(data);
+        expect(expectedResult).to.be.equal(6);
+    });
+
+    it("should detect delimiter and split with it", function() {
+        var data = "//;\n1;2";
+        var expectedResult = stringCalculator.Add(data);
+        expect(expectedResult).to.be.equal(3);
+    });
 });
